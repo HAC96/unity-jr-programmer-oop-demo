be9b9fc89fd1d27395da004288410eae4d040dbf
HAC96 <104535854+HAC96@users.noreply.github.com>
2023-05-11 19:22:27 +0100

    basic-behaviours
    - all creatures can move and attack
    - player controlled by input controls
    - bat can see past and fly over obstacles
    - orc deals double damage when low on health
    - skeleton has chance to resurrect
    - slime splits into pair of smaller, weaker copies

---
+++ Assets/Scripts/Bat.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bat : Monster // INHERITANCE
+{
+    protected override int MaxHitPoints { get => maxHitPoints; }
+    protected override float MoveSpeed { get => moveSpeed; }
+    protected override float AttackRange { get => attackRange; }
+    protected override float AttackCooldown { get => attackCooldown; }
+    protected override float MinDamage { get => minDamage; }
+    protected override float MaxDamage { get => maxDamage; }
+    protected override float DetectionRange { get => detectionRange; }
+
+    [SerializeField] int maxHitPoints = 15;
+    [SerializeField] float moveSpeed = 4;
+    [SerializeField] float attackRange = 1;
+    [SerializeField] float attackCooldown = 0.25f;
+    [SerializeField] float minDamage = 1;
+    [SerializeField] float maxDamage = 5;
+    [SerializeField] float detectionRange = 6;
+
+    // POLYMORPHISM
+    protected override LayerMask mask { get => LayerMask.GetMask("Player"); }
+}
---
+++ Assets/Scripts/Orc.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Orc : Monster // INHERITANCE
+{
+    protected override int MaxHitPoints { get => maxHitPoints; }
+    protected override float MoveSpeed { get => moveSpeed; }
+    protected override float AttackRange { get => attackRange; }
+    protected override float AttackCooldown { get => attackCooldown; }
+    protected override float MinDamage { get => isRaging ? minDamage * 2 : minDamage; }
+    protected override float MaxDamage { get => isRaging ? maxDamage * 2 : maxDamage; }
+    protected override float DetectionRange { get => detectionRange; }
+
+    [SerializeField] int maxHitPoints = 50;
+    [SerializeField] float moveSpeed = 2;
+    [SerializeField] float attackRange = 1;
+    [SerializeField] float attackCooldown = 0.75f;
+    [SerializeField] float minDamage = 4;
+    [SerializeField] float maxDamage = 12;
+    [SerializeField] float detectionRange = 6;
+
+    protected bool isRaging = false;
+
+    protected void Update()
+    {
+        if (HitPoints < MaxHitPoints / 2)
+        {
+            isRaging = true;
+            Debug.Log($"{gameObject.name} became enraged");
+        }
+    }
+}
